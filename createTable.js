f99104ddcb95906b5ff897d1d972c60259e54612
di4m0nds <ws.2000.sja@gmail.com>
2022-02-22 19:10:56 -0300

    Create Table Template
    Set params and function create table

---
+++ createTable.js
@@ -0,0 +1,34 @@
+const params = {
+  TableName: "Movies",
+  KeySchema: [
+    { AttributeName: "year", KeyType: "HASH" },
+    { AttributeName: "title", KeyType: "RANGE" }
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "year", AttributeType: "N" },
+    { AttributeName: "title", AttributeType: "S" }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+}
+
+const createTable = async (dynamodb) => {
+  await dynamodb.createTable(params, (err, data) => {
+    if (err) {
+      console.error(
+        "Unable to create table. Error JSON:\n\n",
+        JSON.stringify(err, null, 2)
+      );
+    }
+    else {
+      console.log(
+        "Created table. Table description JSON:\n\n",
+        JSON.stringify(data, null, 2)
+      );
+    }
+  })
+}
+
+export { createTable }
