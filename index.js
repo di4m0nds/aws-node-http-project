af4da85f75ac344a64925a675687e945c471ecd6
di4m0nds <ws.2000.sja@gmail.com>
2022-02-22 19:52:15 -0300

    Added New Funtion
    Function, Create new item

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ import 'dotenv/config'
 
 import { createTable } from './createTable.js'
 import {
+    createItem,
   loadAllMovies
 } from './functions.js'
 
+// AWS Configuration
 const AWS_CONFING = {
   region: process.env.AWS_REGION,
   endpoint: process.env.AWS_ENDPOINT
 }
 AWS.config.update(AWS_CONFING)
 
+// Create table in dynamodb
 const dynamodb = new AWS.DynamoDB()
 createTable(dynamodb)
 
 // Functions
 const docClient = new AWS.DynamoDB.DocumentClient()
 loadAllMovies(docClient)
+createItem(docClient)
